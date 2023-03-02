96275ab900949fea2b48e1da77dc2c3c8bf200be
Cemil Özen <coezen73@gmail.com>
2023-03-02 15:20:43 +0100

    Commit from sdet1

--- src/test/java/tests/test_1.java
+++ src/test/java/tests/test_1.java
@@ -2,6 +2,7 @@ package tests;
 
 public class test_1 {
     public static void main(String[] args) {
+        //Changed something
         System.out.println("Hello");
     }
 }
---
+++ src/test/java/tests/test_2_sdet1.java
@@ -0,0 +1,7 @@
+package tests;
+
+public class test_2_sdet1 {
+    public static void main(String[] args) {
+        // some lines. . .
+    }
+}

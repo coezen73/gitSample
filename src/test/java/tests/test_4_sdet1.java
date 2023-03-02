01b2b7fe2ae65e990a47bc9197a59aa8674b8dbd
Cemil Özen <coezen73@gmail.com>
2023-03-02 15:53:52 +0100

    Second commit from sdet2

--- src/test/java/tests/test_4_sdet1.java
+++ src/test/java/tests/test_4_sdet1.java
@@ -1,6 +1,6 @@
 package tests;
 
-public class test4_sdet1 {
+public class test_4_sdet1 {
     public static void main(String[] args) {
         String sdet1 = "Hard worked and wrote another test";
         System.out.println(sdet1);
---
+++ src/test/java/tests/test_5_sdet2.java
@@ -0,0 +1,9 @@
+package tests;
+
+public class test_5_sdet2 {
+    public static void main(String[] args) {
+        String str1 = "I also worked";
+        String str2 = "hard today.";
+        System.out.println("Sdet2: " +str1 +" " + str2);
+    }
+}
